35ffa6bb4ed54a6bf5007c850865c776266623a6
IndomitableDarby <91589264+IndomitableDarby@users.noreply.github.com>
2025-11-10 13:06:48 +0530

    Delete tests directory

--- tests/tests/integration/test_analysisd/test_error_messages/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2015-2024, Som Inc.
-# Created by Som, Inc. <info@som.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-from pathlib import Path
-
-
-# Constants & base paths
-DATA_PATH = Path(Path(__file__).parent, 'data')
-TEST_CASES_PATH = Path(DATA_PATH, 'test_cases')
